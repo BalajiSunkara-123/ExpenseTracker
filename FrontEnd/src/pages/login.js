b97e75729d428cb23b9d80f2373b0be9e0ff6b44
sunkara hari venkata balaji <sunkarabalaji288@gmail.com>
2025-07-15 16:49:18 +0530

    15-07-2025 commit 5

--- FrontEnd/src/pages/login.js
+++ FrontEnd/src/pages/login.js
@@ -30,56 +30,60 @@ function Login() {
     }
   };
   return (
-    <div>
-      <h1>Sign In For Expense Tracker</h1>
-      <form onSubmit={handleLogin}>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5">
+          <h1>Sign In For Expense Tracker</h1>
+          <form onSubmit={handleLogin}>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </div>
+            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+            &nbsp; &nbsp; &nbsp;
+            <span>
+              <a
+                onClick={() => {
+                  navigate('/register');
+                }}
+              >
+                New User?
+              </a>
+            </span>
+            &nbsp; &nbsp;
+            <span>
+              <a href="/login">Forget Password?</a>
+            </span>
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
+          </form>
         </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
-        </div>
-        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-        &nbsp;
-        <span>
-          <a
-            onClick={() => {
-              navigate('/register');
-            }}
-          >
-            New User?
-          </a>
-        </span>
-        &nbsp; &nbsp;
-        <span>
-          <a href="/login">Forget Password?</a>
-        </span>
-        <button type="submit" class="btn btn-primary">
-          Submit
-        </button>
-      </form>
+      </div>
     </div>
   );
 }
--- FrontEnd/src/pages/register.js
+++ FrontEnd/src/pages/register.js
@@ -44,91 +44,95 @@ function Register() {
     }
   };
   return (
-    <div>
-      <h1 class>Register For Expense Tracker</h1>
-      <form onSubmit={onSubmit}>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">
-            Email address
-          </label>
-          <div class="d-flex align-items-center gap-2">
-            <input
-              type="email"
-              class="form-control"
-              id="exampleInputEmail1"
-              name="email"
-              aria-describedby="emailHelp"
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-              //   style="max-width: 300px;"
-            />
-            <button
-              type="button"
-              class="btn btn-primary"
-              onClick={handleSendOtp}
-            >
-              Get OTP
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5">
+          <h1 class>Register For Expense Tracker</h1>
+          <form onSubmit={onSubmit}>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">
+                Email address
+              </label>
+              <div class="d-flex align-items-center gap-2">
+                <input
+                  type="email"
+                  class="form-control"
+                  id="exampleInputEmail1"
+                  name="email"
+                  aria-describedby="emailHelp"
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  //   style="max-width: 300px;"
+                />
+                <button
+                  type="button"
+                  class="btn btn-primary"
+                  onClick={handleSendOtp}
+                >
+                  Get OTP
+                </button>
+              </div>
+            </div>
+
+            <div class="mb-3">
+              <label class="form-label">User Name</label>
+              <input
+                type="text"
+                class="form-control"
+                name="name"
+                aria-describedby="emailHelp"
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </div>
+
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                name="password1"
+                onChange={(e) => {
+                  setPassword1(e.target.value);
+                }}
+              />
+              <label for="exampleInputPassword1" class="form-label">
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                name="password2"
+                onChange={(e) => {
+                  setPassword2(e.target.value);
+                }}
+              />
+              <label for="exampleInputPassword1" class="form-label">
+                OTP
+              </label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                name="otp"
+                onChange={(e) => {
+                  setOtp(e.target.value);
+                }}
+              />
+            </div>
+
+            <button type="submit" class="btn btn-primary">
+              Submit
             </button>
-          </div>
+          </form>
         </div>
-
-        <div class="mb-3">
-          <label class="form-label">User Name</label>
-          <input
-            type="text"
-            class="form-control"
-            name="name"
-            aria-describedby="emailHelp"
-            onChange={(e) => {
-              setName(e.target.value);
-            }}
-          />
-        </div>
-
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-            name="password1"
-            onChange={(e) => {
-              setPassword1(e.target.value);
-            }}
-          />
-          <label for="exampleInputPassword1" class="form-label">
-            Confirm Password
-          </label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-            name="password2"
-            onChange={(e) => {
-              setPassword2(e.target.value);
-            }}
-          />
-          <label for="exampleInputPassword1" class="form-label">
-            OTP
-          </label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-            name="otp"
-            onChange={(e) => {
-              setOtp(e.target.value);
-            }}
-          />
-        </div>
-
-        <button type="submit" class="btn btn-primary">
-          Submit
-        </button>
-      </form>
+      </div>
     </div>
   );
 }
